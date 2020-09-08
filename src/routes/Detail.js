d578681bfcf125cf11b91aa98396efa8e7bf07a2
Jeong_geumdam <suseongland@gmail.com>
2020-09-08 19:06:15 +0900

    Update Deploying to Github Pages

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Detail.css"
+import "./Detail.css";
 
 class Detail extends React.Component {
   componentDidMount() {
@@ -10,18 +10,26 @@ class Detail extends React.Component {
   }
   render() {
     const { location } = this.props;
-    console.log(location)
     if (location.state) {
       return (
         <div className="detail__container">
-          <img className="detail__image" src={location.state.large_image} alt={location.state.title} title={location.state.title} />
+          <img
+            className="detail__image"
+            src={location.state.large_image}
+            alt={location.state.title}
+            title={location.state.title}
+          />
           <div>
-            <h1 className="detail__title">{location.state.title}({location.state.year})</h1>
-            <h4 className="detail__genre">장르:{location.state.genres.join('/')}</h4>
+            <h1 className="detail__title">
+              {location.state.title}({location.state.year})
+            </h1>
+            <h4 className="detail__genre">
+              장르:{location.state.genres.join("/")}
+            </h4>
             <p className="detail__summary">{location.state.summary}</p>
           </div>
         </div>
-      )
+      );
     } else {
       return null;
     }
